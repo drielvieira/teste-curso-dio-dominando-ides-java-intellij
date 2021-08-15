1dbf6f0965ca151e833c20045845678b07ad1d9d
adriel vieira <adriel532vieira@gmail.com>
2021-08-15 19:41:39 -0300

    crianndo class livro seu tolo

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,9 +8,43 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        // Deu uma treta aqui que não faço ideia do que foi
+        Livro livro = new Livro("Deu ruim", 350);
+        System.out.println(livro);
 
+    }
+}
 
+class Livro {
+    private String nome;
+    private Integer  numPaginas;
 
-       /* System.out.printf("Hello world");*/
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
